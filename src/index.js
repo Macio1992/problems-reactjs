d16bad5c038a37e1b8491cbe35b72d182121448e
Maciej Zbierowski <maix19bep@gmail.com>
2020-11-24 17:14:03 +0100

    branch4: fixes

--- src/index.js
+++ src/index.js
@@ -6,12 +6,6 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './Reducers';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-// import { createLogger } from 'redux-logger';
-
-// const store = createStore(
-//   reducer,
-//   applyMiddleware(thunk),
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
